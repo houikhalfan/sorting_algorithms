36951b66712e2f0a495412518ef6cc0a67b52a9c
houikhalfan <h.ouikhalfan@gmail.com>
2024-05-06 16:59:26 +0100

    advv

--- sort.h
+++ sort.h
@@ -24,6 +24,13 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-
+void merge_subarr(int *subarr, int *buff, size_t front, size_t mid, size_t back);
+void merge_sort_recursive(int *subarr, int *buff, size_t front, size_t back);
+void merge_sort(int *array, size_t size);
+int get_max(int *array, int size);
+void counting_sort(int *array, size_t size);
+void swap_node_ahead(listint_t **list, listint_t **tail, listint_t **shaker);
+void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker);
+void cocktail_sort_list(listint_t **list);
 
 #endif /* SORT_H */
